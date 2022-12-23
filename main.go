c26bd8bd12673a5c9206e572f902fbceb7aff5e2
Gabriel <gabriel@MacBook.local>
2022-12-23 00:47:45 -0800

    Struct

--- main.go
+++ main.go
@@ -1,10 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Cadastro struct {
+	Nome     string
+	Idade    int
+	Endereco string
+	Saldo    float64
+}
 
 func main() {
 
 	Api := "Api rest com Golang"
 	fmt.Println(Api)
+
+	Gabriel := Cadastro{"Gabriel", 36, "Miami", 645.67}
+
+	GabrielJson, err := json.Marshal(Gabriel)
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Println(string(GabrielJson))
 
 }
