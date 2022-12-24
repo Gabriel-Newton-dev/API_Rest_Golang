fc63b456a8736b6fc35e0dc2f1dc2ff513e08dda
Gabriel <gabriel@MacBook.local>
2022-12-23 21:29:34 -0800

    configurando struct

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,12 +1,19 @@
 package controllers
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/Gabriel-Newton-dev/API_Rest_Golang/models"
 )
 
 // Home toda vez que chegar uma requisicao Home, a func irá escrever writer (w), a String que colocamos que no caso é "Home Page"
 func Home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Home Page")
 
+}
+
+func TodasPersonalidades(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(models.Personalidade)
 }
--- routes/routes.go
+++ routes/routes.go
@@ -9,7 +9,7 @@ import (
 
 // HandleRequest(lhe dar com requisição) convenção de utilizar
 func HandleRequest() {
-	http.HandleFunc("/", controllers.Home) // handleFunc - lhe dar com a função.
-	// toda vez que ele receber um "/"" ele irá executar a nossa função Home.
+	http.HandleFunc("/", controllers.Home) // handleFunc - lhe dar com a função. // toda vez que ele receber um "/"" ele irá executar a nossa função Home.
+	http.HandleFunc("/personalidades", controllers.TodasPersonalidades)
 	log.Fatal(http.ListenAndServe(":7500", nil))
 }
